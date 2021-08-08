9f5218feecb095f7f55a6ae179bf55713c45c8fa
Artem Mamchenko <mavdss2006@gmail.com>
2021-08-08 18:03:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0, j = 0; i <= 15; i += 3, j += 5)
+            {
+                Console.WriteLine("i = " + i);
+                Console.WriteLine("j = " + j);
+
+                sum += i;
+
+                Console.WriteLine("Sum after adding i  = " + sum);
+
+                if (j < 995 && j % 3 != 0)
+                {
+                    sum += j;
+                }
+
+                Console.WriteLine("Sum after adding j  = " + sum);
+
+            }
+        
+        }
+
+    }
+}
